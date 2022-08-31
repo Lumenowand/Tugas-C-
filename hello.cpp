79f6cb1457aadf42f509bea104bcdedb5eb483f1
Lumenowand <111964134+Lumenowand@users.noreply.github.com>
2022-08-31 16:17:33 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+	cout << "Nama : Wanda Ramdani"<<endl;
+	cout << "Kelas : X PPLG 1"<<endl;
+	cout << "==========================="<<endl;
+	cout << "Angka1 =10"<<endl;
+	cout << "Angka2 =5"<<endl;
+	cout << "hasil:"<< 10*5 << endl;
+	return 0;
+}
